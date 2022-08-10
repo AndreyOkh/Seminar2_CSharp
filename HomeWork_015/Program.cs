da23bdc6830a4b5af51b15c9eff2a6150975dcd1
Андрей Охотников <andrey1q2q@gmail.com>
2022-08-10 16:44:21 +1000

    Написал программу которая на вход получает число обозначающее день недели и на выход отвечает соответствует ли данное число выходному

---
+++ HomeWork_015/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+int number = 0;
+
+number = ReadInt("Введите число: ");
+
+if (number == 6 || number == 7)
+{
+    Console.WriteLine("Да");
+}
+else if (number > 0 && number < 6)
+{
+    Console.WriteLine("Нет");
+}
+else 
+{
+    Console.WriteLine("Введеное число не соответствует дням недели.");
+}
+
+/*--------------------Functions--------------------*/
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
