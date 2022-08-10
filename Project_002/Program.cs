773c35c057cac4973f47bc50b0df9a9918328d43
Андрей Охотников <andrey1q2q@gmail.com>
2022-08-10 10:35:41 +1000

    Написали программу которая берет рандомное число из диапазонаа, разделяет его на составляющие и выводит наибольший элемент

---
+++ Project_002/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+// 78 -> 8 
+// 12-> 2 
+// 85 -> 8
+
+int randomNumber = new Random().Next(10, 100);
+
+int firstDigit = randomNumber / 10;
+int secondDigit = randomNumber % 10;
+
+Console.WriteLine(randomNumber);
+
+if (firstDigit < secondDigit) 
+{
+    Console.WriteLine(secondDigit);
+}
+else if (firstDigit > secondDigit)
+{
+    Console.WriteLine(firstDigit);
+}
+else
+{
+    Console.WriteLine("Значения равны");
+}
