c9fc8ee8da8108f42655c355241365664e93822f
Андрей Охотников <andrey1q2q@gmail.com>
2022-08-10 10:45:27 +1000

    Перенесли поиск максимального числа в функцию Max

--- Project_002/Program.cs
+++ Project_002/Program.cs
@@ -10,20 +10,23 @@ int secondDigit = randomNumber % 10;
 
 Console.WriteLine(randomNumber);
 
-if (firstDigit < secondDigit) 
-{
-    Console.WriteLine(secondDigit);
-}
-else if (firstDigit > secondDigit)
-{
-    Console.WriteLine(firstDigit);
-}
-else
-{
-    Console.WriteLine("Значения равны");
-}
+Console.WriteLine(Max(firstDigit, secondDigit));
 
 int GetRandomNumber(int min, int max)
 {
     return new Random().Next(min, max + 1);
+}
+
+int Max (int a, int b)
+{
+    int max = 0;
+    if (a > b) 
+    {
+        max = a;
+    }
+    else
+    {
+        max = b;
+    }
+    return max;
 }
